8cb75662df84381e002ac52e562b82f270c453b1
Artur Harutyunyan <Arturhar@outlook.com>
2022-09-09 18:37:42 +0400

    init

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ slider.addEventListener('input', function(e) {
     passwordLength.textContent = value
     val = value
     value = (this.value-this.min)/(this.max-this.min)*100
-    this.style.background = 'linear-gradient(to right, #a3ffaf 0%, #a3ffaf ' + value + '%, #fff ' + value + '%, white 100%)'
+    this.style.background = 'linear-gradient(to right, #a3ffaf 0%, #a3ffaf ' + value + '%, #08070be6 ' + value + '%, #08070be6 100%)'
 })
 

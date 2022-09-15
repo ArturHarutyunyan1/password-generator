aa2be0cabcef53c45a607c5ebbbdc411b1db1c58
Artur Harutyunyan <Arturhar@outlook.com>
2022-09-15 17:47:47 +0400

    init

--- script.js
+++ script.js
@@ -1,11 +1,94 @@
-const slider = document.querySelector('.slider')
+const result         = document.querySelector('.password')
+const slider         = document.querySelector('.slider')
 const passwordLength = document.querySelector('.value')
+const generator      = document.querySelector('.generator-btn') 
+const flags          = document.querySelectorAll('.flag')
 
-slider.addEventListener('input', function(e) {
+let upperCase = false
+let lowerCase = false
+let numbers   = false
+let symbols   = false
+
+let chars    = ''
+let password = ''
+slider.addEventListener('input', sliderFunction)
+generator.addEventListener('click', passwordGenerator)
+
+function sliderFunction(e){
     let value = e.target.value
     passwordLength.textContent = value
-    val = value
     value = (this.value-this.min)/(this.max-this.min)*100
     this.style.background = 'linear-gradient(to right, #a3ffaf 0%, #a3ffaf ' + value + '%, #08070be6 ' + value + '%, #08070be6 100%)'
+}
+
+flags.forEach(flag => {
+    flag.addEventListener('click', (e)=>{
+        let target   = e.target
+        if(!target.hasAttribute('checked')){
+            target.setAttribute('checked', '')
+        }else if(target.hasAttribute('checked')){
+            target.removeAttribute('checked')
+        }
+    })
 })
 
+function passwordGenerator(){
+    let length = slider.value
+
+    flags.forEach(flag => {
+        let id = flag.id
+        
+        if(flag.hasAttribute('checked')){
+            if(id == 'upperCase'){
+                upperCase = true
+            }
+            if(id == 'lowerCase'){
+                lowerCase = true
+            }
+            if(id == 'numbers'){
+                numbers = true
+            }
+            if(id == symbols){
+                symbols = true
+            }
+        }else{
+            if(id == 'upperCase'){
+                upperCase = false
+            }
+            if(id == 'lowerCase'){
+                lowerCase = false
+            }
+            if(id == 'numbers'){
+                numbers = false
+            }
+            if(id == symbols){
+                symbols = false
+            }
+        }
+    })
+
+    if(upperCase){
+        chars += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'       
+    }
+    if(lowerCase){
+        chars += 'abcdefghijklmnopqrstuvwxyz'
+    }
+    if(numbers){
+        chars += '0123456789'
+    }
+    if(symbols){
+        chars += '!@#$%^&*()_+/.";[]{}|-='
+    }
+
+    for(let i = 0; i <= length; i++){
+        let random = Math.floor(Math.random() * chars.length)
+        password += chars.substring(random, random +1);
+    }
+
+    result.textContent = password
+    
+    upperCase = false
+    lowerCase = false
+    numbers   = false
+    symbols   = false
+}
